502994be25878febfc7a92c8da5b545e3642acaa
gowthamDepaul <55822808+gowthamDepaul@users.noreply.github.com>
2021-01-22 11:32:27 -0600

    localupdate
    lcup

--- epi_judge_java/epi/ReverseDigits.java
+++ epi_judge_java/epi/ReverseDigits.java
@@ -4,26 +4,36 @@ import epi.test_framework.GenericTest;
 public class ReverseDigits {
   @EpiTest(testDataFile = "reverse_digits.tsv")
   public static long reverse(int x) { //don't use arrays, constant space complexity O(1) //dont use string builder
-   System.out.println(x);
-   StringBuilder strNum = new StringBuilder(); //string concatenation runtime O(n^2), string builder O(n), immutability
-   System.out.println("teststring");
-   String temp = Integer.toString(x);
-   System.out.println(temp);
-  // int[] newGuess = new int[temp.length()];
-  int counter=0;
-   for (int i = temp.length()-1; i >-1; i--)
-   {
-    
-       //newGuess[counter] = temp.charAt(i) - '0';
-       
-      // System.out.println(newGuess[counter]);
-       strNum.append(temp.charAt(i) - '0');
-       counter+=1;
+   int inputnum=x;
+   int finalval=0;
+   while (inputnum>0){
+     finalval =finalval*10;
+     finalval=finalval+inputnum%10;
+     inputnum=inputnum/10;
+     System.out.println(inputnum);
+
    }
-   int finalInt=Integer.parseInt(strNum.toString());
+
    
-   System.out.println(finalInt);
-   return finalInt;
+  //  StringBuilder strNum = new StringBuilder(); //string concatenation runtime O(n^2), string builder O(n), immutability
+  //  System.out.println("teststring");
+  //  String temp = Integer.toString(x);
+  //  System.out.println(temp);
+  // // int[] newGuess = new int[temp.length()];
+  // int counter=0;
+  //  for (int i = temp.length()-1; i >-1; i--)
+  //  {
+    
+  //      //newGuess[counter] = temp.charAt(i) - '0';
+       
+  //     // System.out.println(newGuess[counter]);
+  //      strNum.append(temp.charAt(i) - '0');
+  //      counter+=1;
+  //  }
+  //  int finalInt=Integer.parseInt(strNum.toString());
+   
+  //  System.out.println(finalInt);
+   return finalval;
   }
 
   public static void main(String[] args) {
