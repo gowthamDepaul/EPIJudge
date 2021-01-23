c5b5acb8fde93a7c8a58431ebb220d65fc9f273a
gowthamDepaul <55822808+gowthamDepaul@users.noreply.github.com>
2021-01-23 09:34:44 -0600

    Lcup
    lcup

--- epi_judge_java/epi/ReverseDigits.java
+++ epi_judge_java/epi/ReverseDigits.java
@@ -4,8 +4,11 @@ import epi.test_framework.GenericTest;
 public class ReverseDigits {
   @EpiTest(testDataFile = "reverse_digits.tsv")
   public static long reverse(int x) { //don't use arrays, constant space complexity O(1) //dont use string builder
-   int inputnum=x;
-   int finalval=0;
+  int inputnum=x;
+  if(x<0){
+    inputnum=-(inputnum);
+  }
+   long finalval=0;
    while (inputnum>0){
      finalval =finalval*10;
      finalval=finalval+inputnum%10;
@@ -13,26 +16,9 @@ public class ReverseDigits {
      System.out.println(inputnum);
 
    }
-
-   
-  //  StringBuilder strNum = new StringBuilder(); //string concatenation runtime O(n^2), string builder O(n), immutability
-  //  System.out.println("teststring");
-  //  String temp = Integer.toString(x);
-  //  System.out.println(temp);
-  // // int[] newGuess = new int[temp.length()];
-  // int counter=0;
-  //  for (int i = temp.length()-1; i >-1; i--)
-  //  {
-    
-  //      //newGuess[counter] = temp.charAt(i) - '0';
-       
-  //     // System.out.println(newGuess[counter]);
-  //      strNum.append(temp.charAt(i) - '0');
-  //      counter+=1;
-  //  }
-  //  int finalInt=Integer.parseInt(strNum.toString());
-   
-  //  System.out.println(finalInt);
+   if(x<0){
+    return -finalval;
+   }
    return finalval;
   }
 
