86e0b2b6e0eb7b4848ad654b558bf8a72e5a7c5d
gowthamDepaul <55822808+gowthamDepaul@users.noreply.github.com>
2021-01-29 17:45:13 -0600

    update
    update

--- epi_judge_java/epi/IntAsArrayMultiply.java
+++ epi_judge_java/epi/IntAsArrayMultiply.java
@@ -10,12 +10,16 @@ public class IntAsArrayMultiply {
     //List<Integer> intlist= new List<Integer>;
    // intlist.add(0);
    ArrayList<Integer> finalinteger = new ArrayList<>();
+   List<Integer> finalpushinteger = new ArrayList<>();
     int carryover=0;
+    int firstcounter=0;
     for (int i=num1.size()-1; i>0;i--){
+
       for (int j=num2.size()-1;j>0;j--){
         int f1=num1.get(i);
         int f2=num2.get(j);
         int multf=(f1*f2)+carryover;
+        carryover=0;
         int length = String.valueOf(multf).length();
         if(length>1){
           String temp = Integer.toString(multf);
@@ -34,6 +38,25 @@ public class IntAsArrayMultiply {
        // finalmulholder.add(5, 3);
 
       }
+      if(firstcounter==0){
+    
+
+      finalpushinteger.addAll(finalinteger);
+      firstcounter+=1;
+      continue;
+    }
+    if(firstcounter>0){
+      int sizeofexsistingfinal=finalinteger.size();
+      int tempobj=finalpushinteger.remove(sizeofexsistingfinal-1);
+      finalpushinteger.addAll(finalinteger);
+      int lenoffinal=finalpushinteger.size();
+      finalpushinteger.add(lenoffinal,tempobj);
+
+
+
+      //find length of exsisting list]
+    }
+
     }
     return null;
   }
